2b339643cf82a79bfafbde2885e2478c74a40af4
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2023-07-19 17:58:42 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,45 @@
 <template>
   <footer>
-    <Logo />
-    <div class="social">
-      <Icon icon="fa-brands fa-youtube" />
-      <Icon icon="fa-brands fa-facebook" />
-      <Icon icon="fa-brands fa-instagram" />
-    </div>
+    <!-- logo and contact details -->
+    <section class="logo">
+      <div class="logo-wrapper">
+        <Logo :show_info="true" />
+      </div>
+      <div :class="['info', { 'top-24': device != 'desktop' }]">
+        <div class="info-row">
+          <Icon class="mini-icon" icon="fa-solid fa-envelope" />
+          <p>gozzofrancesco@gmail.com</p>
+        </div>
+        <div class="info-row">
+          <Icon class="mini-icon" icon="fa-solid fa-phone" />
+          <p>+3489357118</p>
+        </div>
+        <div class="info-row">
+          <Icon class="mini-icon" icon="fa-solid fa-address-card" />
+          <p>P.IVA: 04508650274</p>
+        </div>
+        <div class="info-row">
+          <Icon class="mini-icon" icon="fa-solid fa-location-pin" />
+          <p>Mira(VE) 30034</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- social network -->
+    <section class="social">
+      <a href="https://www.facebook.com/fgozzo1">
+        <Icon icon="fa-brands fa-facebook" />
+      </a>
+      <a href="https://www.youtube.com/channel/UC-lmsrehL8zl0B6uoqHz-xw">
+        <Icon icon="fa-brands fa-youtube" />
+      </a>
+      <a href="https://www.instagram.com/kg_creator/">
+        <Icon icon="fa-brands fa-instagram" />
+      </a>
+    </section>
+
+    <!-- sign -->
+    <span class="sign">Nicolò Cavalli  &copy; {{ new Date().getFullYear() }}</span>
   </footer>
 </template>
 
@@ -13,15 +47,20 @@
 // ==============================
 // Import
 // ==============================
-import { getViewport } from "../utils/screen_size";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import {
+  faYoutube,
+  faFacebook,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { getViewport } from '../utils/screen_size'
 import Logo from './Logo.vue'
+
 
 // ==============================
 // Consts
 // ==============================
-library.add( faYoutube, faFacebook, faInstagram );
+library.add(faYoutube, faFacebook, faInstagram);
 const device = getViewport();
 
 
@@ -30,13 +69,47 @@ const device = getViewport();
 <style lang="scss" scoped>
 footer {
   width: 100%;
-  height: 30rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: var(--footer-bg);
-  .social {
+  padding: 0 0 10rem 0;
+  section.logo {
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+    border-bottom: 0.1rem solid rgba(255, 255, 255, 0.1);
+    margin: 3rem 0;
+    padding: 4.5rem 0;
+    justify-content: center;
+    .logo-wrapper {
+      width: 50%;
+      flex-basis: 30rem;
+      min-width: 20rem;
+      display: grid;
+      place-content: center;
+    }
+    .info {
+      width: 50%;
+      flex-basis: 30rem;
+      min-width: 20rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      .info-row {
+        display: flex;
+        align-items: center;
+        padding: 0.1rem;
+      }
+      .mini-icon {
+        width: 1.2rem;
+        height: 1.2rem;
+        margin-right: 1.2rem;
+      }
+    }
+  }
+  section.social {
     margin-top: 2rem;
     width: 20rem;
     display: flex;
@@ -44,9 +117,17 @@ footer {
     justify-content: space-around;
     text-align: center;
     svg {
-      width: 3rem;
-      height: 3rem;
+      width: 2.5rem;
+      height: 2.5rem;
+      padding: 1.2rem;
+      border: 0.1rem solid rgba(255, 255, 255, 0.1);
+      border-radius: 50%;
     }
   }
+}
+
+span.sign {
+  padding-top: 4rem;
+  font-size: 0.8rem;
 }
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="logo" @click="$router.push('/')">
-    <h4>KG</h4>
+  <div class="wrapper">
+    <div :class="['logo', { 'c-margin' : show_info }]" @click="$router.push('/')">
+      <h4>KG</h4>
+    </div>
+    <div v-if="show_info" class="top-12">
+      <p class="c-text name">Francesco Gozzo</p>
+      <p class="c-text">Art director and producer</p>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// ==============================
+// Import
+// ==============================
+defineProps({
+  show_info: Boolean,
+})
+</script>
 
 <style lang="scss" scoped>
-.logo {
-  width: max-content;
-  display: grid;
-  place-content: center;
-  user-select: none;
-  background-color: black;
-  border-radius: 50%;
-  width: 5rem;
-  height: 5rem;
+.wrapper {
+  .logo {
+    width: max-content;
+    display: grid;
+    place-content: center;
+    user-select: none;
+    background-color: black;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+  }
+}
+.name {
+  font-weight: bold;
+  font-size: 1.6rem;
 }
 </style>
